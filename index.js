cc820a71e863cc3972d9c812f9a13236344237de
tanveercoder <142831424+tanveercoder@users.noreply.github.com>
2024-07-02 03:24:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,45 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-
-dotenv.config();
-
-// Resolving dirname for ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
+import path from "path";
+
 const app = express();
-
-app.use(express.static(path.join(__dirname, '/Client/dist')));
-
-// Render client for any path
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/Client/dist/index.html')));
 
 app.use(cors());
 app.use(express.json());
 
+dotenv.config();
+
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 
-// Connect to MongoDB
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch(error => {
+// connect to mongoDB
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
     console.log("Error: ", error);
-});
+}
 
-// Defining routes
+// defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+
+
+if (process.env.MODE_ENV === "production"){
+    const dirPath=path.resolve();
+    app.use(express.static("Frontend/dist"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(dirPath,"Frontend","dist","index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
